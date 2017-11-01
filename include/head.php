17927652ce4e139e521e5c1c06e13641d80a808a
fallykaramoko <karamoko.gu16r0010@glocal.university>
2017-11-01 19:09:46 +0530

    admin

---
+++ include/head.php
@@ -0,0 +1,37 @@
+
+<html>
+<head>
+<title>BCA 5th semester Major project</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Augment Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+ <!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<!-- Custom CSS -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- Graph CSS -->
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- jQuery -->
+<link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css'>
+<!-- lined-icons -->
+<link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
+<!-- //lined-icons -->
+<script src="js/jquery-1.10.2.min.js"></script>
+<script src="js/amcharts.js"></script>
+<script src="js/serial.js"></script>
+<script src="js/light.js"></script>
+<script src="js/radar.js"></script>
+<link href="css/barChart.css" rel='stylesheet' type='text/css' />
+<link href="css/fabochart.css" rel='stylesheet' type='text/css' />
+<!--clock init-->
+<script src="js/css3clock.js"></script>
+<!--Easy Pie Chart-->
+<!--skycons-icons-->
+<script src="js/skycons.js"></script>
+
+<script src="js/jquery.easydropdown.js"></script>
+
+<!--//skycons-icons-->
+</head>
---
+++ include/statistics.php
@@ -0,0 +1,39 @@
+<div class="charts" style="margin-left:300px">
+  <div class="chrt-inner">
+    <div class="chrt-bars">
+    <div class="col-md-6 chrt-two">
+     <center><h3 class="sub-tittle">Statistics </center></h3>
+      <div id="chart2"></div>
+      <script src="js/fabochart.js"></script>
+      <script>
+      $(document).ready(function () {
+        data = {
+          'BCA' : 300,
+          'Bcom' : 200,
+          'LAW' : 100,
+          'CIVIL' : 500,
+          'PETROLIUM' : 400,
+          'CS' : 200
+        };
+
+        $("#chart1").faBoChart({
+          time: 500,
+          animate: true,
+          instantAnimate: true,
+          straight: false,
+          data: data,
+          labelTextColor : "#002561",
+        });
+        $("#chart2").faBoChart({
+          time: 2500,
+          animate: true,
+          data: data,
+          straight: true,
+          labelTextColor : "#002561",
+        });
+      });
+      </script>
+    </div>
+</div>
+</div>
+  </div>
